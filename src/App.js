20a62c1025b940557db52019a39ac5a1fa90b55e
parksaneon <sangeon1500@daum.net>
2022-02-15 20:38:15 +0900

    feat : make common button component

--- src/App.js
+++ src/App.js
@@ -6,10 +6,22 @@ import New from './pages/New';
 import Edit from './pages/Edit';
 import Diary from './pages/Diary';
 
+// Components
+import MyButton from './components/MyButton';
+
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <h2>App.js</h2>
+
+        <MyButton text={'버튼'} type={'positive'} />
+        <img src={process.env.PUBLIC_URL + '/assets/emotion1.png'} alt="" />
+        <img src={process.env.PUBLIC_URL + '/assets/emotion2.png'} alt="" />
+        <img src={process.env.PUBLIC_URL + '/assets/emotion3.png'} alt="" />
+        <img src={process.env.PUBLIC_URL + '/assets/emotion4.png'} alt="" />
+        <img src={process.env.PUBLIC_URL + '/assets/emotion5.png'} alt="" />
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
