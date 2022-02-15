2473895b21a806ca073a7ce375a943396e03b2ca
parksaneon <sangeon1500@daum.net>
2022-02-15 19:51:44 +0900

    import useNavigate from react-router-dom

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,6 +1,7 @@
-import { useSearchParams } from 'react-router-dom';
+import { useSearchParams, useNavigate } from 'react-router-dom';
 
 const Edit = () => {
+  const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
 
   const id = searchParams.get('id');
@@ -11,6 +12,7 @@ const Edit = () => {
       <h1>Edit</h1>
       <p>일기 수정 페이지 입니다.</p>
       <button onClick={() => setSearchParams({ who: 'Park' })}>QS 바꾸기</button>
+      <button onClick={() => navigate('/home')}>Home으로 가기</button>
     </div>
   );
 };
