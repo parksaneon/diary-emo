5ee626151ed7a7462f8c8793c0b6f7e096098afa
ParkSangEon <sangeon1500@daum.net>
2022-02-17 10:54:49 +0900

    feat : header in home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,29 @@
-const Home = () => (
-  <div>
-    <h1>Home</h1>
-  </div>
-);
+import { useState } from 'react';
+import MyButton from '../components/MyButton';
+
+import MyHeader from '../components/MyHeader';
+
+const Home = () => {
+  const [curDate, setCurDate] = useState(new Date());
+  const headerText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
+
+  const decreaseMonth = () => {
+    setCurDate(new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate()));
+  };
+
+  const increaseMonth = () => {
+    setCurDate(new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate()));
+  };
+
+  return (
+    <div>
+      <MyHeader
+        headerText={headerText}
+        leftChild={<MyButton text={'<'} onClick={decreaseMonth} />}
+        rightChild={<MyButton text={'>'} onClick={increaseMonth} />}
+      />
+    </div>
+  );
+};
 
 export default Home;
