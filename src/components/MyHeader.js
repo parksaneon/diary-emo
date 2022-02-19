88d1a7ba52ee9211fa88003e82ff90ddaffbc5d2
ParkSangEon <sangeon1500@daum.net>
2022-02-19 18:38:01 +0900

    feat : back button of new page

--- src/components/MyHeader.js
+++ src/components/MyHeader.js
@@ -1,9 +1,9 @@
 const MyHeader = ({ headerText, leftChild, rightChild }) => {
   return (
     <header>
-      <div className="head_btn_left">{leftChild}</div>
-      <div className="head_text">{headerText}</div>
-      <div className="head_btn_right">{rightChild}</div>
+      <div className='head_btn_left'>{leftChild}</div>
+      <div className='head_text'>{headerText}</div>
+      <div className='head_btn_right'>{rightChild}</div>
     </header>
   );
 };
--- src/pages/New.js
+++ src/pages/New.js
@@ -1,7 +1,15 @@
-const New = () => (
-  <div>
-    <h1>New</h1>
-  </div>
-);
+import { useNavigate } from 'react-router-dom';
+
+import MyHeader from '../components/MyHeader';
+import MyButton from '../components/MyButton';
+
+const New = () => {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <MyHeader headerText='새 일기 쓰기' leftChild={<MyButton text='< 뒤로가기' onClick={() => navigate(-1)} />} />
+    </div>
+  );
+};
 
 export default New;
