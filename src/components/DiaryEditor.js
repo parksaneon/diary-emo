0662a441de1187b749e94537a9f58b57279bd987
ParkSangEon <sangeon1500@daum.net>
2022-02-19 18:48:08 +0900

    feat : DiaryEditor Component

---
+++ src/components/DiaryEditor.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import MyHeader from './MyHeader';
+import MyButton from './MyButton';
+
+const getStringDate = date => date.toISOString().slice(0, 10);
+
+const DiaryEditor = () => {
+  const [date, setDate] = useState(getStringDate(new Date()));
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <MyHeader headerText='새 일기 쓰기' leftChild={<MyButton text='< 뒤로가기' onClick={() => navigate(-1)} />} />
+      <div>
+        <section>
+          <h4>오늘은 언제인가요?</h4>
+          <div className='input-box'>
+            <input className='input-date' type='date' onChange={e => setDate(e.target.value)} />
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default DiaryEditor;
--- src/pages/New.js
+++ src/pages/New.js
@@ -1,26 +1,9 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-
-import MyHeader from '../components/MyHeader';
-import MyButton from '../components/MyButton';
-
-const getStringDate = date => date.toISOString().slice(0, 10);
+import DiaryEditor from '../components/DiaryEditor';
 
 const New = () => {
-  const [date, setDate] = useState(getStringDate(new Date()));
-  const navigate = useNavigate();
-
   return (
     <div>
-      <MyHeader headerText='새 일기 쓰기' leftChild={<MyButton text='< 뒤로가기' onClick={() => navigate(-1)} />} />
-      <div>
-        <section>
-          <h4>오늘은 언제인가요?</h4>
-          <div className='input-box'>
-            <input className='input-date' type='date' onChange={e => setDate(e.target.value)} />
-          </div>
-        </section>
-      </div>
+      <DiaryEditor />
     </div>
   );
 };
