e813221b4108c71e01faf4b4484bf60583a88ee5
ParkSangEon <sangeon1500@daum.net>
2022-02-19 19:01:47 +0900

    feat : DiaryEditor add Emotion Array

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -4,6 +4,14 @@ import { useNavigate } from 'react-router-dom';
 import MyHeader from './MyHeader';
 import MyButton from './MyButton';
 
+const emotionList = [
+  { emotion_id: 1, emotion_img: `${process.env.PUBLIC_URL}/assets/emotion1.png`, emotion_descript: '완전 좋음' },
+  { emotion_id: 2, emotion_img: `${process.env.PUBLIC_URL}/assets/emotion2.png`, emotion_descript: '좋음' },
+  { emotion_id: 3, emotion_img: `${process.env.PUBLIC_URL}/assets/emotion3.png`, emotion_descript: '그럭저럭' },
+  { emotion_id: 4, emotion_img: `${process.env.PUBLIC_URL}/assets/emotion4.png`, emotion_descript: '나쁨' },
+  { emotion_id: 5, emotion_img: `${process.env.PUBLIC_URL}/assets/emotion5.png`, emotion_descript: '끔직함' }
+];
+
 const getStringDate = date => date.toISOString().slice(0, 10);
 
 const DiaryEditor = () => {
@@ -18,6 +26,14 @@ const DiaryEditor = () => {
           <h4>오늘은 언제인가요?</h4>
           <div className='input-box'>
             <input className='input-date' type='date' onChange={e => setDate(e.target.value)} />
+          </div>
+        </section>
+        <section>
+          <h4>오늘의 감정</h4>
+          <div className='input-box emotion_list_wrapper'>
+            {emotionList.map(({ emotion_id, emotion_img, emotion_descript }) => (
+              <div key={emotion_id}>{emotion_descript}</div>
+            ))}
           </div>
         </section>
       </div>
